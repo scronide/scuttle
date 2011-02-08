caf1635c2eaa3df402edbb8f962b175d04d355bc
Michiel Dethmers <michiel@michiel-laptop.(none)>
2011-02-08 15:51:16 -0300

    added listing of untagged bookmarks

--- templates/toolbar.inc.php
+++ templates/toolbar.inc.php
@@ -8,6 +8,7 @@ if ($userservice->isLoggedOn()) {
     <ul id="navigation">
         <li><a href="<?php echo createURL('bookmarks', $cUsername); ?>"><?php echo T_('Bookmarks'); ?></a></li>
         <li><a href="<?php echo createURL('watchlist', $cUsername); ?>"><?php echo T_('Watchlist'); ?></a></li>
+        <li><a href="<?php echo createURL('untagged', $cUsername); ?>"><?php echo T_('Untagged'); ?></a></li>
         <li><a href="<?php echo createURL('bookmarks', $cUsername . '?action=add'); ?>"><?php echo T_('Add a Bookmark'); ?></a></li>
         <li class="access"><a href="<?php echo $GLOBALS['root']; ?>?action=logout"><?php echo T_('Log Out'); ?></a></li>
     </ul>
